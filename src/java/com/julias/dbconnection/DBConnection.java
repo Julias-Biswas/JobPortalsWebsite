645d7f25934ea1ac56e288d39353baad81f1ce2e
Julias Biswas <juliasbiswas0001@gmail.com>
2022-07-18 00:35:21 +0530

    Initial commit

--- src/java/com/julias/dbconnection/DBConnection.java
+++ src/java/com/julias/dbconnection/DBConnection.java
@@ -1,9 +1,8 @@
 package com.julias.dbconnection;
 
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
-import java.io.InputStream;
 import java.sql.Connection;
-import java.util.Properties;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /*
 * static factory design pattern
@@ -15,43 +14,12 @@ public class DBConnection
     
     public static Connection getConnect() 
     {
-        String jdbc_url = null , username = null , password = null;
-        
         try
         {
-            InputStream is = DBConnection.class.getResourceAsStream("db.properties");
-            
-            Properties p = new Properties();
-            p.load(is);
-            
-            jdbc_url = p.getProperty("jdbc-url");
-            username = p.getProperty("username");
-            password = p.getProperty("password");
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jobportal", "root", "root");
         }
-        catch(Exception ee)
-        {
-            ee.printStackTrace();
-        }
-        
-        try
-        {
-            //Class.forName("com.mysql.jdbc.Driver");
-            //con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jobportal", "root", "root");
-            
-            /*
-            * Using Connection Pooling
-            */
-            
-            MysqlDataSource msds = new MysqlDataSource();
-            
-            msds.setURL(jdbc_url);
-            msds.setUser(username);
-            msds.setPassword(password);
-            
-            con = msds.getConnection();
-            
-        }
-        catch(Exception e)
+        catch(ClassNotFoundException | SQLException e)
         {
             e.printStackTrace();
         }
